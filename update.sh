4960455e928673b944dcd6bf91ba34c32335d2f4
MichaelChuai <18alexanderm117@tongji.edu.cn>
2018-04-18 10:41:49 +0800

    m

---
+++ update.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+cp gconda.Dockerfile build-conda/Dockerfile
+cp gtf.Dockerfile build-tf/Dockerfile
+cp special/work.Dockerfile build-work/Dockerfile
+cd build-conda && docker build -t gconda:5.1.0 -t gconda ./
+cd ../build-tf/ && docker build -t gtf:1.7.0 -t gtf ./
+cd ../build-work/ && docker build -t work ./
